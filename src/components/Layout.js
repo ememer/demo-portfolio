92fc619038537987d96735ca27aabc183ac46f6e
ememer <46853050+ememer@users.noreply.github.com>
2022-02-06 15:26:38 +0100

    Adjust html structure and CSS

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
 
+import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 
 import clsx from "clsx";
+import Scroll from "react-scroll";
+
+import { mediaLinks, navigationLinks } from "../shared/utils/navigation";
 
 import Hamburger from "./Hamburger";
 
+const ScrollLink = Scroll.Link;
+
 const Layout = ({ children }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className="min-h-screen p-2 text-gray-200 bg-gradient-to-r from-mainDark-200 to-mainDark-300">
+    <div
+      onClick={(e) => {
+        if (e.target.id !== "hamburger") {
+          setIsMenuOpen(false);
+        }
+      }}
+      id="/"
+      className="min-h-screen p-2 text-gray-200 bg-gradient-to-r from-mainDark-200 to-mainDark-300"
+    >
       <div className="container flex flex-row justify-end md:justify-between items-center py-10 px-8 mx-auto">
         <div className="w-full text-center lg:text-left">
           <StaticImage
@@ -22,13 +37,55 @@ const Layout = ({ children }) => {
         {isMenuOpen && (
           <nav
             className={clsx(
-              "fixed top-0 left-0 z-50 w-0 min-h-screen py-20 px-10 text-2xl font-bold text-center bg-mainDark-200 rounded-r-lg shadow-md animate-menu"
+              "fixed top-0 left-0 z-50 w-0 min-h-screen py-20 px-10 text-2xl font-semibold text-center bg-mainDark-200 rounded-r-lg shadow-md animate-menu"
             )}
           >
-            <ul>
-              <li>test</li>
-              <li>test</li>
+            <ul className="grid grid-cols-1 space-y-5">
+              <Link className="text-3xl font-bold" to="/">
+                HOME
+              </Link>
+              {navigationLinks.map((nav, index) => (
+                <li key={index}>
+                  <ScrollLink
+                    onClick={() => setIsMenuOpen(false)}
+                    to={nav.url}
+                    duration={300}
+                    delay={500}
+                    smooth={true}
+                    className="flex flex-col mt-4 md:mt-8 mb-2 md:mb-4 cursor-pointer"
+                  >
+                    {nav.link}
+                  </ScrollLink>
+                  {nav.projects && (
+                    <ScrollLink
+                      onClick={() => setIsMenuOpen(false)}
+                      to={nav.projects.url}
+                      duration={300}
+                      delay={500}
+                      smooth={true}
+                      className="font-light cursor-pointer"
+                    >
+                      {nav.projects.link}
+                    </ScrollLink>
+                  )}
+                </li>
+              ))}
             </ul>
+            <div className="my-20">
+              <ul className="grid grid-cols-2">
+                {mediaLinks.map((externalLink) => (
+                  <a
+                    className="px-6 text-sm"
+                    target="_blank"
+                    key={externalLink.link}
+                    href={externalLink.url}
+                    rel="noreferrer"
+                  >
+                    {externalLink.link}
+                  </a>
+                ))}
+              </ul>
+            </div>
           </nav>
         )}
       </div>
---
+++ src/shared/utils/navigation.js
@@ -0,0 +1,42 @@
+const navigationLinks = [
+  {
+    link: "DESIGN",
+    url: "/",
+    projects: {
+      link: "PROJECTS",
+      url: "/",
+    },
+  },
+
+  {
+    link: "DEVELOPING",
+    url: "/",
+    projects: {
+      link: "PROJECTS",
+      url: "/",
+    },
+  },
+  {
+    link: "CONTACT",
+    url: "/",
+  },
+  {
+    link: "RESUME",
+    url: "/",
+  },
+];
+
+const mediaLinks = [
+  {
+    link: "Github",
+    url: "https://www.github.com/ememer",
+    icon: "XYZ",
+  },
+  {
+    link: "Dribbble",
+    url: "https://www.dribbble.com/ememer",
+    icon: "ZYX",
+  },
+];
+
+export { mediaLinks, navigationLinks };
